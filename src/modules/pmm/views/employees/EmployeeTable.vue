34aad60a4917607886e47a2d55c8a91454c665e5
sahosisocial <sahos@isocial.com.bd>
2022-04-05 09:44:02 +0600

    remove all table filer

--- src/modules/pmm/views/employees/EmployeeTable.vue
+++ src/modules/pmm/views/employees/EmployeeTable.vue
@@ -12,36 +12,6 @@
           <th>Employee ID</th>
           <th class="col-serial">Action</th>
         </tr>
-        <tr>
-          <th class="col-serial">
-            <input type="checkbox" @click="checkAll()" v-model="isCheckAll" />
-            All
-          </th>
-          <th>
-            <input
-              type="text"
-              :value="nameSearch"
-              class="form-input"
-              @input="$emit('update:nameSearch', $event.target.value)"
-            />
-          </th>
-          <th>
-            <select
-              class="form-input"
-              :value="isActiveSearch"
-              @input="$emit('update:isActiveSearch', $event.target.value)"
-            >
-              <option value="">Choose</option>
-              <option value="1">Active</option>
-              <option value="0">In-Active</option>
-            </select>
-          </th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th class="col-serial"></th>
-        </tr>
       </thead>
       <tbody :class="{ tableLoader: $attrs.loadingState }">
         <tr v-for="(td, index) in $attrs.entries" :key="td">

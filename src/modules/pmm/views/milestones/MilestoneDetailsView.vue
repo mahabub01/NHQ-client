10243fa3725f9f729ebe6e29ed85f110892e1df7
sahosisocial <sahos@isocial.com.bd>
2022-05-18 10:51:24 +0600

    boq, oem, poc details page

---
+++ src/modules/pmm/views/milestones/MilestoneDetailsView.vue
@@ -0,0 +1,225 @@
+<template>
+  <div class="container-fluid">
+    <!--start Spinner -->
+    <the-spinner
+      :isSaving="savingSpinner"
+      :is-loading="loadingSpinner"
+    ></the-spinner>
+
+    <div class="form-bootcamp">
+      <div class="row">
+        <div class="col-md-4">
+          <router-link to="/pmm/milestones"
+            >Milestone List <i class="fas fa-chevron-right"></i
+          ></router-link>
+          <router-link to="#">Milestone Details</router-link>
+        </div>
+        <div class="col-md-8"></div>
+      </div>
+    </div>
+    <div class="form-design-body">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 offset-md-3">
+            <h4 class="form-page-title details-header-title">
+              Milestone Details
+            </h4>
+            <div class="row">
+              <div class="col-md-6">
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Milestone ID</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.milestone_id }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Milestone Name</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.milestone_name }}
+                  </p>
+                </div>
+                <!--end-->
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Project Name</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.project_name }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Milestone Category</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.categoryMilestone }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Assign Employee</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.assign_employee }}
+                  </p>
+                </div>
+                <!--end-->
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Follow Up</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.follow_up }}
+                  </p>
+                </div>
+                <!--end-->
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Points</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.points }}
+                  </p>
+                </div>
+                <!--end-->
+              </div>
+              <div class="col-md-6">
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Start Date</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.start_date }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">End Date</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.end_date }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Extended Date</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.extended_date }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Milstone Description</h5>
+                  <div
+                    class="des"
+                    v-if="getInformation != null"
+                    v-html="getInformation.description"
+                  ></div>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Status</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.status }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Milsetone Files</h5>
+                  <a
+                    target="_blank"
+                    v-if="getInformation != null"
+                    :href="`${getInformation.files}`"
+                    >Download File</a
+                  >
+                </div>
+                <!--end-->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+
+import Axios from "@/http-common";
+import toastr from "toastr";
+import TheSpinner from "../../../shared/spinners/TheSpinner.vue";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+import { useRouter } from "vue-router";
+
+//use for saving preloader
+let savingSpinner = ref(false);
+let loadingSpinner = ref(false);
+
+//use route
+const route = useRoute();
+const router = useRouter();
+
+//create store
+const store = useStore();
+
+//Load Data form computed onMounted
+onMounted(() => {
+  loadSingleData();
+  store.commit("modalModule/LOAD_CKEDITOR_MODAL", false);
+});
+
+//get Files
+const getInformation = ref(null);
+
+//Load Single Data
+async function loadSingleData() {
+  loadingSpinner.value = true;
+  await Axios.get("/milestones-details/" + route.params.slug).then(
+    (response) => {
+      console.log(response);
+      loadingSpinner.value = false;
+      if (response.data.code === 200) {
+        getInformation.value = response.data.data;
+      } else {
+        toastr.error(response.data.message);
+      }
+    }
+  );
+}
+</script>
+
+<style scoped>
+.details-header-title {
+  text-align: left;
+  font-size: 20px;
+  font-weight: 700;
+}
+
+.data-label {
+  font-size: 14px;
+  font-weight: 600;
+  margin-bottom: 3px;
+}
+.data-text {
+  color: #6b778c;
+  font-size: 12px;
+}
+
+.des p {
+  color: #6b778c;
+  font-size: 12px;
+}
+</style>
---
+++ src/modules/pmm/views/oems/OemDetailsView.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="container-fluid">
+    <!--start Spinner -->
+    <the-spinner
+      :isSaving="savingSpinner"
+      :is-loading="loadingSpinner"
+    ></the-spinner>
+
+    <div class="form-bootcamp">
+      <div class="row">
+        <div class="col-md-4">
+          <router-link to="/pmm/oems"
+            >Oem List <i class="fas fa-chevron-right"></i
+          ></router-link>
+          <router-link to="#">Oem Details</router-link>
+        </div>
+        <div class="col-md-8"></div>
+      </div>
+    </div>
+    <div class="form-design-body">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 offset-md-3">
+            <h4 class="form-page-title details-header-title">Oem Details</h4>
+            <div class="row">
+              <div class="col-md-6">
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Oem ID</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.oem_id }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Oem Title</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.oem_title }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Oem Version</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.version }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Project Name</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.project_title }}
+                  </p>
+                </div>
+                <!--end-->
+              </div>
+              <div class="col-md-6">
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Oem Description</h5>
+                  <div
+                    class="des"
+                    v-if="getInformation != null"
+                    v-html="getInformation.description"
+                  ></div>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Point</h5>
+                  <div
+                    class="des"
+                    v-if="getInformation != null"
+                    v-html="getInformation.point"
+                  ></div>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Oem Status</h5>
+                  <p class="data-text" v-if="getInformation != null">
+                    {{ getInformation.status }}
+                  </p>
+                </div>
+                <!--end-->
+
+                <!--start-->
+                <div>
+                  <h5 class="data-label">Oem Files</h5>
+                  <a
+                    target="_blank"
+                    v-if="getInformation != null"
+                    :href="`${getInformation.files}`"
+                    >Download File</a
+                  >
+                </div>
+                <!--end-->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+
+import Axios from "@/http-common";
+import toastr from "toastr";
+import TheSpinner from "../../../shared/spinners/TheSpinner.vue";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+import { useRouter } from "vue-router";
+
+//use for saving preloader
+let savingSpinner = ref(false);
+let loadingSpinner = ref(false);
+
+//use route
+const route = useRoute();
+const router = useRouter();
+
+//create store
+const store = useStore();
+
+//Load Data form computed onMounted
+onMounted(() => {
+  loadSingleData();
+  store.commit("modalModule/LOAD_CKEDITOR_MODAL", false);
+});
+
+//get Files
+const getInformation = ref(null);
+
+//Load Single Data
+async function loadSingleData() {
+  loadingSpinner.value = true;
+  await Axios.get("/projects/oems-details/" + route.params.slug).then(
+    (response) => {
+      console.log(response);
+      loadingSpinner.value = false;
+      if (response.data.code === 200) {
+        getInformation.value = response.data.data;
+      } else {
+        toastr.error(response.data.message);
+      }
+    }
+  );
+}
+</script>
+
+<style scoped>
+.details-header-title {
+  text-align: left;
+  font-size: 20px;
+  font-weight: 700;
+}
+
+.data-label {
+  font-size: 14px;
+  font-weight: 600;
+  margin-bottom: 3px;
+}
+.data-text {
+  color: #6b778c;
+  font-size: 12px;
+}
+
+.des p {
+  color: #6b778c;
+  font-size: 12px;
+}
+</style>
